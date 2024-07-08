99ee2070e41a842f9b5bce74d821eaab4099daa2
ArrayList021013 <3075378602@qq.com>
2024-07-08 17:12:19 +0800

    project config success

---
+++ src/preloads/preload.ts
@@ -0,0 +1,20 @@
+import { contextBridge } from 'electron';
+
+window.addEventListener("DOMContentLoaded", () => {
+    const replaceText = (selector: string, text: string) => {
+        const element = document.getElementById(selector);
+        if (element) {
+            element.innerText = text;
+        }
+    }
+
+    for (const dependency of ['chrome', 'node', 'electron']) {
+        replaceText(`${dependency}-version`, (process.versions as any)[dependency]);
+    }
+});
+
+contextBridge.exposeInMainWorld('versions', {
+    node: () => process.versions.node,
+    chrome: () => process.versions.chrome,
+    electron: () => process.versions.electron
+});
